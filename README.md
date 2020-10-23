e015987c91025b15a2abe4d1141d511248f4d087
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2020-10-23 16:40:42 +0100

    Introduce deployment script
    The deployment script should make it easer for users to get started with the
    route service.

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ can be set with the `cf set-env` command.
 If your CF deployment has a self-signed SSL certificate, set the
 `SKIP_SSL_VALIDATION` environment variable to avoid SSL errors when proxying to
 the backend.
+
+## Deployment script
+You can deploy this route service to GOV.UK PaaS using the deployment in `deploy.sh`. It will not work outside of GOV.UK PaaS, because of some assumptions made about domain names.
+
+To run the script, see the example below:
+```shell script
+AUTH_USERNAME=username \
+AUTH_PASSWORD=password \
+ROUTE_SERVICE_APP_NAME=name_of_this_app \
+ROUTE_SERVICE_NAME=name_to_give_the_route_service \
+PROTECTED_APP_NAME=name_of_the_app_to_protect_with_basic_auth \
+./deploy.sh
+```
---
+++ deploy.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v jq >/dev/null; then
+  echo "Must have JQ installed"
+  exit 1
+fi
+
+if [ -z "${AUTH_USERNAME+set}" ]; then
+  echo "Must set AUTH_USERNAME environment variable"
+  exit 1
+fi
+
+if [ -z "${AUTH_PASSWORD+set}" ]; then
+  echo "Must set AUTH_PASSWORD environment variable"
+  exit 1
+fi
+
+if [ -z "${ROUTE_SERVICE_APP_NAME+set}" ]; then
+  echo "Must set ROUTE_SERVICE_APP_NAME environment variable"
+  exit 1
+fi
+
+if [ -z "${ROUTE_SERVICE_NAME+set}" ]; then
+  echo "Must set ROUTE_SERVICE_NAME environment variable"
+  exit 1
+fi
+
+if [ -z "${PROTECTED_APP_NAME+set}" ]; then
+  echo "Must set PROTECTED_APP_NAME environment variable"
+  exit 1
+fi
+
+APPS_DOMAIN=$(cf curl "/v3/domains" | jq -r '[.resources[] | select(.name|endswith("apps.digital"))][0].name')
+
+cf push "${ROUTE_SERVICE_APP_NAME}" --no-start
+cf set-env "${ROUTE_SERVICE_APP_NAME}" AUTH_USERNAME "${AUTH_USERNAME}"
+cf set-env "${ROUTE_SERVICE_APP_NAME}" AUTH_PASSWORD "${AUTH_PASSWORD}"
+cf start "${ROUTE_SERVICE_APP_NAME}"
+
+set -x
+
+ROUTE_SERVICE_DOMAIN="$(cf curl "/v3/apps/$(cf app "${ROUTE_SERVICE_APP_NAME}" --guid)/routes" | jq -r --arg APPS_DOMAIN "${APPS_DOMAIN}" '[.resources[] | select(.url | endswith($APPS_DOMAIN))][0].url')"
+
+if cf curl "/v3/service_instances?type=user-provided&names=${ROUTE_SERVICE_NAME}" | jq -e '.pagination.total_results == 0' > /dev/null; then
+  cf create-user-provided-service \
+    "${ROUTE_SERVICE_NAME}" \
+    -r "https://${ROUTE_SERVICE_DOMAIN}";
+else
+  cf update-user-provided-service \
+    "${ROUTE_SERVICE_NAME}" \
+    -r "https://${ROUTE_SERVICE_DOMAIN}";
+fi
+
+PROTECTED_APP_HOSTNAME="$(cf curl "/v3/apps/$(cf app "${PROTECTED_APP_NAME}" --guid)/routes" | jq -r --arg APPS_DOMAIN "${APPS_DOMAIN}" '[.resources[] | select(.url | endswith($APPS_DOMAIN))][0].host')"
+
+cf bind-route-service "${APPS_DOMAIN}" "${ROUTE_SERVICE_NAME}" --hostname "${PROTECTED_APP_HOSTNAME}";
+
+set +x
